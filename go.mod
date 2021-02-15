3db2ee1fd64c377a15ae223f7d40737a59482186
bkpeh <bkpeh10@yahoo.com>
2021-02-15 18:03:35 +0800

    Add Dynamodb Connection [150221:1803]

--- go.mod
+++ go.mod
@@ -3,6 +3,10 @@ module github.com/bkpeh/wgo
 go 1.15
 
 require (
+	github.com/aws/aws-sdk-go-v2 v1.2.0
+	github.com/aws/aws-sdk-go-v2/config v1.1.1
+	github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue v1.0.2 // indirect
+	github.com/aws/aws-sdk-go-v2/service/dynamodb v1.1.1
 	github.com/golang/protobuf v1.4.3
 	google.golang.org/grpc v1.35.0
 	google.golang.org/protobuf v1.25.0
--- main.go
+++ main.go
@@ -5,8 +5,13 @@ import (
 	"fmt"
 	"net"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
 	datamsg "github.com/bkpeh/wgo/proto"
 	"google.golang.org/grpc"
+	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 type transaction struct {
@@ -14,6 +19,24 @@ type transaction struct {
 }
 
 func (trans transaction) SetTransaction(ctx context.Context, t *datamsg.Transaction) (*datamsg.TransactionReply, error) {
+
+	customResolver := aws.EndpointResolverFunc(func(service, region string) (aws.Endpoint, error) {
+		if service == dynamodb.ServiceID && region == "us-west-2" {
+			return aws.Endpoint{
+				PartitionID:   "aws",
+				URL:           "http://localhost:8000",
+				SigningRegion: "us-west-2",
+			}, nil
+		}
+		// returning EndpointNotFoundError will allow the service to fallback to it's default resolution
+		return aws.Endpoint{}, &aws.EndpointNotFoundError{}
+	})
+
+	cfg, _ := config.LoadDefaultConfig(context.TODO(), config.WithRegion("us-west-2"), config.WithEndpointResolver(customResolver))
+	svc := dynamodb.NewFromConfig(cfg)
+
+	settransactiontoDB(svc)
+
 	tr := &datamsg.TransactionReply{
 		Code: datamsg.TransactionCode_TRANSACTIONSUCCESS,
 	}
@@ -47,4 +70,48 @@ func startserver() {
 	}
 
 	PrintInfo("Server Running...", nil)
+}
+
+func settransactiontoDB(conn *dynamodb.Client) {
+
+	t1 := datamsg.Transaction{
+		TransactionId:   "20210001",
+		TransactionTime: timestamppb.Now(),
+		Customer: &datamsg.Person{
+			Name:    "Peter",
+			Address: "Singapore",
+			Phone:   1234567,
+		},
+		Bookslist: []*datamsg.Books{
+			{
+				Title:   "Speak English",
+				Subject: "English",
+				Price:   10.50,
+				Stream:  datamsg.StreamType_EXPRESS,
+				Level:   datamsg.LevelType_SECONDARY1,
+			},
+			{
+				Title:   "Calculate",
+				Subject: "Maths",
+				Price:   15.80,
+				Stream:  datamsg.StreamType_EXPRESS,
+				Level:   datamsg.LevelType_SECONDARY1,
+			},
+		},
+	}
+
+	item, err := attributevalue.MarshalMap(&t1)
+
+	if err != nil {
+		PrintErr("Cannot MarshalMap", err)
+	}
+
+	_, err = conn.PutItem(context.TODO(), &dynamodb.PutItemInput{
+		TableName: aws.String("TransactionTable"),
+		Item:      item,
+	})
+
+	if err != nil {
+		PrintErr("Cannot PutItem", err)
+	}
 }
